c75945398ce26bec9271ec84dbca84ea3a77ec09
Antoine Goudaillier <antoine_goudaillier@ens.univ-artois.fr>
2023-06-22 13:13:04 +0200

    Ajout style navbar

--- portfolio/src/composants/Navbar.tsx
+++ portfolio/src/composants/Navbar.tsx
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router-dom"
+import "../styles/navbar.css"
 
 export const Navbar = () => {
     return (
         <header>
             <nav>
                 <div className="gauche">
-                    <NavLink to ='/'>Antoine Goudaillier</NavLink>
+                    <p><NavLink to ='/'>Antoine Goudaillier</NavLink></p>
                 </div>
 
                 <div className="droite">
-                    <NavLink to='/projets'>Projets</NavLink>
-                    <NavLink to='/about'>A propos</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
+                    <p><NavLink to='/projets'>Projets</NavLink></p>
+                    <p><NavLink to='/about'>A propos</NavLink></p>
+                    <p><NavLink to='/contact'>Contact</NavLink></p>
                 </div>
             </nav>
         </header>
